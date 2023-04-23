c50f1aa839d1434d5be9f77c6aeaa37218505263
dkkim <dkkim@tecace.com>
2023-04-23 11:52:09 +0900

    ch4. set openai in backend

--- ch4.ColorPaletteGeneratorProject/app.py
+++ ch4.ColorPaletteGeneratorProject/app.py
@@ -1,16 +1,30 @@
+import openai
+from dotenv import dotenv_values
+
 from flask import Flask, render_template, request
+
+config = dotenv_values("../../../.env")
+openai.api_key = config["openai"]
 
 app = Flask(__name__,
     template_folder="templates"
 )
 
-@app.route("/ping")
-def ping():
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    # OpneAi Completion Call
+
+    # Return List of Color
     return "pong!"
 
 @app.route("/")
 def index():
-    return "Hello There from flask!"
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Give me a funny word: "
+    )
+    print('ai test: ', res["choices"][0]["text"])
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
