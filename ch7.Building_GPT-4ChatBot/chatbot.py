c333402a0f1098e7b2388b9c021ca041af4f99c7
dkkim <dkkim@tecace.com>
2023-04-24 22:55:47 +0900

    ch7. Colorizing the chatbot output

--- ch7.Building_GPT-4ChatBot/chatbot.py
+++ ch7.Building_GPT-4ChatBot/chatbot.py
@@ -5,6 +5,22 @@ from dotenv import dotenv_values
 config = dotenv_values("../../../.env")
 openai.api_key = config["openai"]
 
+
+def bold(text):
+    bold_start = "\033[1m"
+    bold_end = "\033[0m"
+    return bold_start + text + bold_end
+
+def blue(text):
+    blue_start = "\033[34m"
+    blue_end = "\033[0m"
+    return blue_start + text + blue_end
+
+def red(text):
+    red_start = "\033[31m"
+    red_end = "\033[0m"
+    return red_start + text + red_end
+
 def main():
     parser = argparse.ArgumentParser(description="Simple command line chatbot with GPT-4")
     parser.add_argument(
@@ -21,14 +37,14 @@ def main():
 
     while True:
         try:
-            user_input = input("You: ")
+            user_input = input(bold(blue("You: ")))
             messages.append({"role": "user", "content": user_input})
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=messages
             )
             messages.append(response["choices"][0]["message"].to_dict())
-            print("Assistant: ", response["choices"][0]["message"]["content"])
+            print(bold(red("Assistant: ")), response["choices"][0]["message"]["content"])
         except KeyboardInterrupt:
             print("Exiting...")
             break
